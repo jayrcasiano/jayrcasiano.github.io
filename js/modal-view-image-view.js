9607dd7f4d083d73473dc32ea2f88fedee6459be
Jay-R Casiano <jayrcasiano.business@gmail.com>
2019-02-04 16:14:05 +0800

    [+] Added modal picture view

---
+++ js/modal-view-image-view.js
@@ -0,0 +1,10 @@
+// function modalImageView() {
+//     $(document).ready(function () {
+//         $(".portfolio-item").click(function (event) {
+//             console.log("ggg");
+//             event.preventDefault();
+//             $(".modal img").attr("src", $(this).attr("href"));
+//             $(".modal").modal("show");
+//         });
+//     });
+// }
--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
+  $(".menu-toggle").click(function (e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
@@ -12,17 +12,17 @@
   });
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
       location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
+      console.log(target);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $("html, body").animate(
-          {
+        $("html, body").animate({
             scrollTop: target.offset().top
           },
           750,
@@ -34,7 +34,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $("#sidebar-wrapper .js-scroll-trigger").click(function() {
+  $("#sidebar-wrapper .js-scroll-trigger").click(function () {
     $("#sidebar-wrapper").removeClass("active");
     $(".menu-toggle").removeClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
@@ -43,7 +43,7 @@
   });
 
   // Scroll to top button appear
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $(".scroll-to-top").fadeIn();
@@ -56,13 +56,13 @@
 // Disable Google Maps scrolling
 // See http://stackoverflow.com/a/25904582/1607849
 // Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
+var onMapMouseleaveHandler = function (event) {
   var that = $(this);
   that.on("click", onMapClickHandler);
   that.off("mouseleave", onMapMouseleaveHandler);
   that.find("iframe").css("pointer-events", "none");
 };
-var onMapClickHandler = function(event) {
+var onMapClickHandler = function (event) {
   var that = $(this);
   // Disable the click handler until the user leaves the map area
   that.off("click", onMapClickHandler);
@@ -82,10 +82,12 @@ var words = new Array(
 );
 var i = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
   function runIt() {
     var baloon = $("#loves-what");
-    baloon.animate({ opacity: "1" }, 1750);
+    baloon.animate({
+      opacity: "1"
+    }, 1750);
     $("#loves-what")
       .empty()
       .append(words[i]);
@@ -94,7 +96,9 @@ $(document).ready(function() {
     } else {
       i = 0;
     }
-    baloon.animate({ opacity: "0" }, 1750, runIt);
+    baloon.animate({
+      opacity: "0"
+    }, 1750, runIt);
   }
   runIt();
 });
