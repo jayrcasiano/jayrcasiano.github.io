2d89bce97d5ecfe4a6eb80ea1ea44a342bed950c
Jay-R Casiano <jayrcasiano.business@gmail.com>
2019-02-05 15:27:44 +0800

    Revert "Revert "[+] Added images""
    This reverts commit 1bfdb05ca34b037b256d97faa8f517991a9239a3.

--- js/modal-view-image-view.js
+++ js/modal-view-image-view.js
@@ -1,10 +1,7 @@
-// function modalImageView() {
-//     $(document).ready(function () {
-//         $(".portfolio-item").click(function (event) {
-//             console.log("ggg");
-//             event.preventDefault();
-//             $(".modal img").attr("src", $(this).attr("href"));
-//             $(".modal").modal("show");
-//         });
-//     });
-// }
+      $(".portfolio-item").click(function (event) {
+          event.preventDefault();
+          //   console.log($(".modal img").attr("src", $(this).attr("href")));
+          $('.modal h4').text($(this).attr("data-modal-title"));
+          $(".modal img").attr("src", $(this).attr("href"));
+          $(".modal").modal("show");
+      });
